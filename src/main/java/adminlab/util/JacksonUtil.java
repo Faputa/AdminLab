82179c41afbc567f69ad8225f43213b1da4f7d35
refcell <2238932729@qq.com>
2021-01-21 14:50:18 +0800

    add JacksonUtil.java

---
+++ src/main/java/adminlab/util/JacksonUtil.java
@@ -0,0 +1,35 @@
+package adminlab.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JacksonUtil {
+
+    private static ObjectMapper objectMapper;
+
+    @Autowired
+    public JacksonUtil(ObjectMapper objectMapper) {
+        JacksonUtil.objectMapper = objectMapper;
+    }
+
+    public static String toJson(Object object) {
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> T toBean(String json, Class<T> valueType) {
+        try {
+            return objectMapper.readValue(json, valueType);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
